92a283dd7a80683e940861e59a6cf2ee688a1fc0
chmeenakshi <meenakshichitikila@gmail.com>
2023-10-16 18:07:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import PasswordManager from './Components/Password/Passwordmanager';
+import Stopwatch from './Components/Stopwatch';
+import FaqItem from './Components/Feqapp/Faqitem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const faqsData = [
+  {
+    id: 0,
+    questionText: 'What is IRC?',
+    answerText:
+      'IRC is an Industry Ready Certification that represents your readiness for a job with the necessary skills.',
+  },
+  {
+    id: 1,
+    questionText: 'What is the medium of instruction?',
+    answerText:
+      'The courses would be delivered in English and Telugu. The program will be available in more vernacular languages soon.',
+  },
+  {
+    id: 2,
+    questionText:
+      'Is there an EMI option to pay the fee for CCBP Tech 4.0 Intensive?',
+    answerText:
+      'Yes, EMI support is available for credit cards. Please select EMI option while making payment for more information.',
+  },
+  {
+    id: 3,
+    questionText: 'How will my doubts be cleared? What is the mechanism?',
+    answerText:
+      'You can ask your doubts in the discussions section, and the course mentor will answer them. You can also see the doubts asked by other students.',
+  },
+];
+
+const App = () => (
+  <div>
+  <div className="feq-container2">
+    <div className="faqs-card-container">
+      <h1 className="heading5">FAQ's</h1>
+      <ul className="faqs-list-container">
+        {faqsData.map(eachFaq => (
+          <FaqItem key={eachFaq.id} faqData={eachFaq} />
+        ))}
+      </ul>
     </div>
-  );
-}
-
-export default App;
+    </div>
+      <Stopwatch/>
+    <PasswordManager/>
+     
+    </div>
+  
+);
+  
+   export default App;
+   
